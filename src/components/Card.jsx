2815154045261408994604b9c3a2bf94b7c8ddf0
vzbel <142253332+vzbel@users.noreply.github.com>
2026-01-11 20:50:26 -0700

    Add game card styles

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,5 @@
-import { LinkIcon, MinusIcon } from '@heroicons/react/24/solid';
+import { LinkIcon } from '@heroicons/react/16/solid';
+import { MinusCircleIcon } from '@heroicons/react/24/outline'; 
 import utils from "../utils/utils.js";
 import "../styles/Card.css";
 
@@ -11,46 +12,62 @@ const Card = ({ image, title, details, tags, onRemoveTag, description }) => {
     const hasTags = tags && utils.isArraySizeInRange(0, 2, tags);
 
     return (
-        <article>
+        <article className="card">
             <div>
-                <img src={image.url} alt={image.alt} />
+                <img 
+                    src={image.url} 
+                    alt={image.alt}
+                    className="card-img" 
+                />
             </div>
+            <hr />
             <div>
-                <div>
-                    <div>
-                        <a href={title.link}>
+                <div className="card-header flex">
+                    <div className="card-header-left">
+                        <a 
+                            href={title.link} 
+                            className="card-header-title flex" 
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
                             <h2>{title.text}</h2>
-                            <LinkIcon width="20" />
+                            <LinkIcon width="18" />
                         </a>
                         {/* An optional maximum of two small details about the item */}
-                        {
-                            hasDetails &&
-                            details.map((detail) => {
-                                const [key, value] = Object.entries(detail)[0];
-                                return (
-                                    <p key={key}>
-                                        {utils.getTitleCase(key)}: {value}
-                                    </p>
-                                );
-                            })
-                        }
+                        <div className="card-details">
+                            {
+                                hasDetails &&
+                                details.map((detail) => {
+                                    const [key, value] = Object.entries(detail)[0];
+                                    return (
+                                        <p key={key} className="card-detail">
+                                            {utils.getTitleCase(key)}: {value}
+                                        </p>
+                                    );
+                                })
+                            }
+                        </div>
                     </div>
-                    <div>
+                    <div className="card-tags">
                         {
                             hasTags && 
                             tags.map((tag) => {
                                 const [key, value] = Object.entries(tag)[0];
                                 return (
-                                    <button key={key} onClick={() => onRemoveTag(tag)}>
-                                        {utils.getTitleCase(key)}: {value}
-                                        <MinusIcon width="16"/>
+                                    <button 
+                                        key={key} 
+                                        onClick={() => onRemoveTag(tag)}
+                                        className="card-tag flex"
+                                    >
+                                        <p>{value}</p>
+                                        <MinusCircleIcon width="18" />
                                     </button>
                                 );
                             })
                         }
                     </div>
                 </div>
-                <p>{description}</p>
+                <p className="card-description">{description}</p>
             </div>
         </article>
     );
